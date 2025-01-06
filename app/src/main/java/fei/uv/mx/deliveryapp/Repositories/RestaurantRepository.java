8a632591a8bd61c07b15ed3cbd75abd3656791af
Miguel Camo <127974100+MigCamo@users.noreply.github.com>
2025-01-05 23:06:14 -0600

    actualizacio rapida

--- app/src/main/java/fei/uv/mx/deliveryapp/Repositories/RestaurantRepository.java
+++ app/src/main/java/fei/uv/mx/deliveryapp/Repositories/RestaurantRepository.java
@@ -45,4 +45,6 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Integer>
 
     @Query("SELECT r.id FROM Restaurant r WHERE r.user.id = ?1")
     int getRestaurantByUserId(int userId);
+
+    List<Restaurant> findByNameRestaurantContainingIgnoreCase(String nameRestaurant);
 }
