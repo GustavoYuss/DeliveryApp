221d4a7a54f541e2471b4be8f95b6c4c40f8c82a
Miguel Camo <127974100+MigCamo@users.noreply.github.com>
2025-01-12 18:48:13 -0600

    contendores

---
+++ drawweb/src/main/java/fei/uv/mx/drawweb/DrawWebApplication.java
@@ -0,0 +1,13 @@
+package fei.uv.mx.drawweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrawWebApplication.class, args);
+    }
+
+}
